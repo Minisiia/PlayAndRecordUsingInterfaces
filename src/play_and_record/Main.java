d68733f3085f69f39cb76d67240af4f789692315
Minisiia <ar_tik@yahoo.com>
2022-11-29 13:38:40 +0200

    Java Essential. 4. Abstract classes and interfaces

--- src/play_and_record/Main.java
+++ src/play_and_record/Main.java
@@ -8,17 +8,5 @@ public class Main {
         player.pause();
         player.stop();
         player.record();
-        System.out.println("Тільки Play");
-        Playable sound1 = new Player();
-        sound1.play();
-        sound1.pause();
-        sound1.stop();
-        //sound1.record(); в інтерфейсі Playable немає методу record();
-        System.out.println("Тільки Record");
-        Recordable sound2 = new Player();
-        sound2.record();
-        sound2.pause();
-        sound2.stop();
-        // sound1.play(); в інтерфейсі Recordable немає методу play();
     }
 }
